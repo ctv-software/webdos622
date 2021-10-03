7dce361534bff0e1ceee9fbeb5ff36357e616620
Maksim <45505876+ctv-software@users.noreply.github.com>
2021-10-03 17:39:07 +0300

    Add files via upload

---
+++ webinstdos.bat
@@ -0,0 +1,42 @@
+@ECHO OFF 
+title instaling MS-DOS 6.22 0%
+
+echo Downloading MS-DOS 6.22
+powershell -command "(New-Object Net.WebClient).DownloadFile('https://github.com/ctv-software/webdos622/raw/main/drivec/DOS.IMA', 'C:\DOS.IMA')" 
+
+cls
+title instaling MS-DOS 6.22 30%
+echo Downloading MS-DOS 6.22..
+echo Downloading Grub4dos.
+powershell -command "(New-Object Net.WebClient).DownloadFile('https://github.com/ctv-software/webdos622/raw/main/drivec/grldr', 'C:\grldr')" 
+cls
+title instaling MS-DOS 6.22 50%
+echo Downloading MS-DOS 6.22..
+echo Downloading Grub4dos..
+powershell -command "(New-Object Net.WebClient).DownloadFile('https://github.com/ctv-software/webdos622/raw/main/drivec/grldr.mbr', 'C:\grldr.mbr')" 
+cls
+title instaling MS-DOS 6.22 60%
+echo Downloading MS-DOS 6.22..
+echo Downloading Grub4dos...
+echo Instling Grub4dos.
+powershell -command "(New-Object Net.WebClient).DownloadFile('https://github.com/ctv-software/webdos622/raw/main/temp/instldr.bat', '%temp%\instldr.bat')" 
+cls
+title instaling MS-DOS 6.22 65%
+echo Downloading MS-DOS 6.22..
+echo Downloading Grub4dos...
+echo Instling Grub4dos..
+call %temp%\instldr.bat
+cls
+echo Downloading MS-DOS 6.22..
+echo Downloading Grub4dos...
+echo Instling Grub4dos...
+echo removing temporary files.
+del %temp%\instldr.bat /q
+cls
+echo Downloading MS-DOS 6.22..
+echo Downloading Grub4dos...
+echo Instling Grub4dos...
+echo removing temporary files..
+echo restart you computer
+shutdown.exe -r -f -t 00
+pause
